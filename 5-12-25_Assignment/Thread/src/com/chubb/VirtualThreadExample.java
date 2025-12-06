ae824dc1ff4b040a84d40d04fd1c71cedb915638
Ishika <22052461@kiit.ac.in>
2025-12-06 10:06:17 +0530

    Security Project From Sratch

---
+++ 5-12-25_Assignment/Thread/src/com/chubb/VirtualThreadExample.java
@@ -0,0 +1,19 @@
+package com.chubb;
+
+public class VirtualThreadExample {
+    public static void main(String[] args) throws InterruptedException {
+        Thread virtualThread =
+            Thread.ofVirtual().name("MyVirtualThread").start(() -> {
+                System.out.println("Virtual thread running: " +
+                    Thread.currentThread().getName());
+                try {
+                    Thread.sleep(1000); // Simulate some work
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            });
+
+        virtualThread.join(); // Wait for the thread to finish
+        System.out.println("Virtual thread finished.");
+    }
+}
